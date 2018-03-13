e14a2d1e1cbcee7e5e959694ab1d8d05fcdedf89
mc.meng <menghaocheng@qq.com>
2018-03-13 16:13:35 +0800

    Add BootCompleteReceiver

--- app/src/main/java/com/justtide/justj/BootCompleteReceiver.java
+++ app/src/main/java/com/justtide/justj/BootCompleteReceiver.java
@@ -9,15 +9,11 @@ public class BootCompleteReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        //throw new UnsupportedOperationException("Not yet implemented");
-        Toast.makeText(context, "Boot Complete", Toast.LENGTH_LONG).show();
-        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
+         if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
             Toast.makeText(context, "Boot Complete", Toast.LENGTH_LONG).show();
             Intent bootSerIntent = new Intent(context, JustjService.class);
             bootSerIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
-            bootSerIntent.setAction("boot_justtide_justj");
+            bootSerIntent.setAction("intent.com.justtide.action.START_JUSTJ");
             context.startService(bootSerIntent);
         }
     }
